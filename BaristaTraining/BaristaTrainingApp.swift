c9ad56387deec0003a4c4312907f02dfe0a3eb82
Ashley Liu <aslorange61@gmail.com>
2021-06-23 22:13:20 -0700

    Initial Commit

---
+++ BaristaTraining/BaristaTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaristaTrainingApp.swift
+//  BaristaTraining
+//
+//  Created by Asl on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct BaristaTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
